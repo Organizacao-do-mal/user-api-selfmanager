735a3c486375257e388f379f8ac0aa522c0428cc
elias.oliveira <elias.oliveira@adviseu.com.br>
2023-11-23 15:25:17 -0300

    initial commit

---
+++ src/test/java/com/selfmanager/userapiselfmanager/UserApiSelfmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfmanager.userapiselfmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiSelfmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
